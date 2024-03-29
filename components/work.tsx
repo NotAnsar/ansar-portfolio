74d0ad7452b13cc8ddd463cab5af4f5455a3d4cf
notansar <karrouach.ansar@gmail.com>
2024-03-29 15:50:07 +0000

    work done

--- components/work.tsx
+++ components/work.tsx
@@ -20,14 +20,12 @@ export default function Work() {
 							width={700}
 							className='w-full h-auto aspect-square z-0'
 						/>
-
-						<div className='absolute top-0 w-full h-full group-hover:bg-black/70 z-[1] transition-all duration-1000' />
-						{/* <div className='absolute bottom-0 w-full h-1/5 group-hover:bg-white/10 z-[1] transition-all duration-1000' /> */}
+						<div className='bg-black/70 sm:bg-transparent absolute top-0 w-full h-full group-hover:bg-black/70 z-[1] transition-all duration-1000' />
 
 						<Link
 							href={w.link}
 							target='_blank'
-							className='hidden group-hover:block absolute right-0 top-0 cursor-pointer z-[2] p-8 bg-foreground/85 rounded-bl-3xl group/link'
+							className='block sm:hidden group-hover:block absolute right-0 top-0 cursor-pointer z-[2] p-8 bg-foreground/85 rounded-bl-3xl group/link'
 						>
 							<svg
 								xmlns='http://www.w3.org/2000/svg'
@@ -46,12 +44,14 @@ export default function Work() {
 							</svg>
 						</Link>
 
-						<div className='absolute p-8 mr-8 opacity-0 bottom-[150px] group-hover:bottom-0 group-hover:opacity-100 transition-all duration-1000 z-[2]'>
-							<h3 className='text-[28px] text-white'>{w.title}</h3>
-							<p className='my-4 text-muted-foreground'>{w.desc}</p>
+						<div className='opacity-100 bottom-0 sm:opacity-0 sm:bottom-[150px] absolute p-8 mr-8  group-hover:bottom-0 group-hover:opacity-100 transition-all duration-1000 z-[2] '>
+							<h3 className='text-2xl sm:text-[28px] text-white'>{w.title}</h3>
+							<p className='my-4 text-sm sm:text-base text-muted-foreground'>
+								{w.desc}
+							</p>
 							<Link
 								href={w.source}
-								className='text-muted-foreground text-[15px] flex gap-2 items-center hover:gap-4 transition-all duration-300 pb-1 border-b-2 border-transparent hover:border-muted-foreground w-fit'
+								className='text-muted-foreground text-[13px] sm:text-[15px] flex gap-2 items-center hover:gap-4 transition-all duration-300 pb-1 border-b-2 border-transparent hover:border-muted-foreground w-fit'
 								target='_blank'
 							>
 								View Code Source
@@ -82,14 +82,15 @@ export default function Work() {
 const WORK = [
 	{
 		title: 'Cadency | Music Web App',
-		desc: 'Cadency is a social media application developed using React.js, Node.js, SCSS, Express.js, and MySQL as the database.',
+		desc: 'Cadency is a music platform developed using cutting-edge technology, including Next.js 14, Tailwind CSS, and Shadcn UI for styling, Prisma with a PostgreSQL database, and NextAuth for authentication.',
+
 		link: 'https://cadency-sage.vercel.app/',
 		image: '/Cadency.png',
 		source: 'https://github.com/NotAnsar/cadency',
 	},
 	{
 		title: 'Connectify | Social Media App',
-		desc: 'Cadency is a music platform developed using cutting-edge technology, including Next.js 14, Tailwind CSS, and Shadcn UI for styling, Prisma with a PostgreSQL database, and NextAuth for authentication.',
+		desc: 'Connectify is a social media application developed using React.js, Node.js, SCSS, Express.js, and MySQL as the database.',
 		link: 'https://www.youtube.com/watch?v=M3_dqIESm5Y&ab_channel=AnsarKarrouach',
 		image: '/Connectify.png',
 		source: 'https://github.com/NotAnsar/connectify-client',
