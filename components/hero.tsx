288e7af21747ec3874d5758e9dc182ddc2403cd4
notansar <karrouach.ansar@gmail.com>
2024-03-29 16:18:01 +0000

    download resume added

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 import Wrapper from './wrapper';
-import { Button } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 import { DownloadIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 export default function Hero() {
 	return (
 		<Wrapper className='grid grid-cols-1 items-center justify-between py-14  md:grid-cols-7'>
 			<div className='md:col-span-4 '>
-				{/* <h3 className='text-4xl my-2 leading-relaxed'> */}
 				<h3 className='text-[2.5rem] sm:text-5xl md:text-7xl my-2 leading-snug sm:leading-normal'>
 					Hi, I am{' '}
 					<span className='font-medium text-main'>Ansar Karrouach</span>
@@ -17,9 +18,14 @@ export default function Hero() {
 					I enjoy creating delightful, <br />
 					human-centered digital experiences.
 				</p>
-				<Button className='flex gap-2 p-6 font-medium'>
+
+				<a
+					href='Karrouach_Ansar_Cv.pdf'
+					download='Karrouach_Ansar_Resume.pdf'
+					className={cn(buttonVariants(), 'flex gap-2 p-6 font-medium w-fit')}
+				>
 					<DownloadIcon /> Resume
-				</Button>
+				</a>
 			</div>
 			<Image
 				alt='hero image'
