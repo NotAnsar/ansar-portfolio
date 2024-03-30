61da183bdfd1c36a4f557a4d78f7f09bd7948e93
notansar <karrouach.ansar@gmail.com>
2024-03-30 03:54:34 +0000

    social links added

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,44 +4,46 @@ import Link from 'next/link';
 
 export default function Footer() {
 	return (
-		<Wrapper className='border-t border-foreground py-8 grid md:grid-cols-4'>
-			<div className='sm:col-span-2 my-4 text-[15px] text-muted-foreground'>
-				<p>© 2021 Ansar.</p>
-				<p>All rights reserved</p>
-			</div>
-			<div className='col-span-1'>
-				<h4>Work with me</h4>
-				<Link
-					href={''}
-					className='text-muted-foreground text-sm hover:opacity-70 transition-all duration-300'
-				>
-					ansar.karrouach@icloud.com
-				</Link>
-			</div>
-			<ul className='col-span-1 mt-4 sm:ml-8 sm:mt-0'>
-				<li>Follow me</li>
-				<Link
-					href={'http://github.com/NotAnsar'}
-					className='text-sm text-muted-foreground block my-1 hover:opacity-70 transition-all duration-300'
-					target='_blank'
-				>
-					Github
-				</Link>
-				<Link
-					href={'https://twitter.com/vnsvrKarrouach'}
-					className='text-sm text-muted-foreground block my-1 hover:opacity-70 transition-all duration-300'
-					target='_blank'
-				>
-					Twitter
-				</Link>
-				<Link
-					href={'https://www.linkedin.com/in/ansarkarrouach/'}
-					className='text-sm text-muted-foreground block my-1 hover:opacity-70 transition-all duration-300'
-					target='_blank'
-				>
-					Linkdin
-				</Link>
-			</ul>
-		</Wrapper>
+		<>
+			<Wrapper className='border-t border-foreground py-8 grid md:grid-cols-4'>
+				<div className='sm:col-span-2 my-4 text-[15px] text-muted-foreground'>
+					<p>© 2021 Ansar.</p>
+					<p>All rights reserved</p>
+				</div>
+				<div className='col-span-1'>
+					<h4>Work with me</h4>
+					<Link
+						href={''}
+						className='text-muted-foreground text-sm hover:opacity-70 transition-all duration-300'
+					>
+						ansar.karrouach@icloud.com
+					</Link>
+				</div>
+				<ul className='col-span-1 mt-4 sm:ml-8 sm:mt-0'>
+					<li>Follow me</li>
+					<Link
+						href={'http://github.com/NotAnsar'}
+						className='text-sm text-muted-foreground block my-1 hover:opacity-70 transition-all duration-300'
+						target='_blank'
+					>
+						Github
+					</Link>
+					<Link
+						href={'https://twitter.com/vnsvrKarrouach'}
+						className='text-sm text-muted-foreground block my-1 hover:opacity-70 transition-all duration-300'
+						target='_blank'
+					>
+						Twitter
+					</Link>
+					<Link
+						href={'https://www.linkedin.com/in/ansarkarrouach/'}
+						className='text-sm text-muted-foreground block my-1 hover:opacity-70 transition-all duration-300'
+						target='_blank'
+					>
+						Linkdin
+					</Link>
+				</ul>
+			</Wrapper>
+		</>
 	);
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,40 +1,41 @@
 import Image from 'next/image';
 import Wrapper from './wrapper';
-import { Button, buttonVariants } from './ui/button';
+import { buttonVariants } from './ui/button';
 import { DownloadIcon } from '@radix-ui/react-icons';
-import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 export default function Hero() {
 	return (
-		<Wrapper className='grid grid-cols-1 items-center justify-between py-14  md:grid-cols-7'>
-			<div className='md:col-span-4 animate-fade-in'>
-				<h3 className='text-[2.5rem] sm:text-5xl md:text-7xl my-2 leading-snug sm:leading-normal'>
-					Hi, I am{' '}
-					<span className='font-medium text-main'>Ansar Karrouach</span>
-					<br /> Web Developer
-				</h3>
-				<p className='text-muted-foreground my-6 text-lg'>
-					I enjoy creating delightful, <br />
-					human-centered digital experiences.
-				</p>
+		<>
+			<Wrapper className='grid grid-cols-1 items-center justify-between py-14  md:grid-cols-7'>
+				<div className='md:col-span-4 animate-fade-in'>
+					<h3 className='text-[2.5rem] sm:text-5xl md:text-7xl my-2 leading-snug sm:leading-normal'>
+						Hi, I am{' '}
+						<span className='font-medium text-main'>Ansar Karrouach</span>
+						<br /> Web Developer
+					</h3>
+					<p className='text-muted-foreground my-6 text-lg'>
+						I enjoy creating delightful, <br />
+						human-centered digital experiences.
+					</p>
 
-				<a
-					href='Karrouach_Ansar_Cv.pdf'
-					download='Karrouach_Ansar_Resume.pdf'
-					className={cn(buttonVariants(), 'flex gap-2 p-6 font-medium w-fit')}
-				>
-					<DownloadIcon /> Resume
-				</a>
-			</div>
-			<Image
-				alt='hero image'
-				src={'/Saly.png'}
-				width={500}
-				height={500}
-				priority
-				className='md:col-span-3 w-full h-auto aspect-square animate-scale-in'
-			/>
-		</Wrapper>
+					<a
+						href='Karrouach_Ansar_Cv.pdf'
+						download='Karrouach_Ansar_Resume.pdf'
+						className={cn(buttonVariants(), 'flex gap-2 p-6 font-medium w-fit')}
+					>
+						<DownloadIcon /> Resume
+					</a>
+				</div>
+				<Image
+					alt='hero image'
+					src={'/Saly.png'}
+					width={500}
+					height={500}
+					priority
+					className='md:col-span-3 w-full h-auto aspect-square animate-scale-in'
+				/>
+			</Wrapper>
+		</>
 	);
 }
